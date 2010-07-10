03b80adddd1993598d0ddb92eb7cf43f72921d21
Quang Anh Do <quanganhdo@gmail.com>
2010-07-10 21:04:54 +0100

    Links to profiles in Activities point directly to built-in Contact viewer

--- Classes/ActivitiesViewController.m
+++ Classes/ActivitiesViewController.m
@@ -54,8 +54,10 @@
 		for (int i = 0; i < numberOfItems; i++) {
 			NSDictionary *entry = [[data objectForKey:@"entry"] objectAtIndex:i];
 			
-			NSString *contact = [NSString stringWithFormat:@"<a href='%@/mods/_standard/social/sprofile.php?id=%d'>%@</a>", kATutorURL, 
-								[[entry objectForKey:@"userId"] integerValue], [contactList objectForKey:[entry objectForKey:@"userId"]]];
+			NSString *contact = [NSString stringWithFormat:@"<a href='atutor://contact/%d/%@'>%@</a>", 
+								[[entry objectForKey:@"userId"] integerValue], 
+								 [[contactList objectForKey:[entry objectForKey:@"userId"]] stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding], 
+								 [contactList objectForKey:[entry objectForKey:@"userId"]]];
 			NSString *xhtml = [NSString stringWithFormat:@"%@ %@", 
 							   contact, // contact
 							   [entry objectForKey:@"title"]]; // title
